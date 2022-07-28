4d152d77b4c1194df261f1bb14bd4d5e807d8ab2
gongchengzhi <joinhack@gmail.com>
2022-07-28 11:09:09 +0800

    s3 support

--- examples/index.ts
+++ examples/index.ts
@@ -7,13 +7,17 @@ let s3_cfg = new S3Configure("fO2ksBwh3YJ6TKm9mJJJ", "5mT0XTyIoXfPxWOo5EDvgnNHgT
 
 let bucketName = "akash-postgres-backup";
 let bucket = new Bucket(bucketName, s3_cfg)
-
+//this is list bucket the parameter is prefix.
 let rs = bucket.list("/");
 Console.log(`${rs!}`);
 
 let fileName = "/test.file" ;
+
+//this is put the data to the path.
 let success = bucket.putObject(fileName, [64,65,66,67,68,69,64,65,66])
 Console.log(`put object: ${success}`)
+
+//this is get the data from the path.
 let obj = bucket.getObject(fileName);
 if (obj != null) {
     let read_string = String.UTF8.decodeUnsafe(obj!.dataStart, obj!.length);
@@ -22,5 +26,6 @@ if (obj != null) {
 rs = bucket.list("/");
 Console.log(`${rs!}`);
 
+//this is delete the file in the bucket.
 success = bucket.deleteObject(fileName)
 Console.log(`delete object: ${success}`)
