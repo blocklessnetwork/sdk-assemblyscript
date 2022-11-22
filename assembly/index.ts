a28bb8883084b9abcee305d298e8588f0cbc9abb
gongchengzhi <gongcz@koal.com>
2022-11-22 10:25:43 +0800

    support cgi

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,7 @@
 import * as http from "./http";
 import * as json from "./json";
+import * as cgi from "./cgi";
 import * as ipfs from "./ipfs";
 import * as memory from "./memory";
 
-export { http, json, ipfs, memory };
+export { http, json, ipfs, memory, cgi };
