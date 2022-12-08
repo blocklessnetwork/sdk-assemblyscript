d9254c018128650a180945402e053f4dc185b7c2
gongchengzhi <gongcz@koal.com>
2022-12-08 20:39:29 +0800

    change data site

--- examples/http.ts
+++ examples/http.ts
@@ -5,7 +5,7 @@ import { Console } from "as-wasi/assembly";
 import { json, http } from "../assembly";
 
 let handle: http.HttpHandle | null = http.HttpOpen(
-  "https://demo.bls.dev/tokens",
+  "http://httpbin.org/anything",
   new http.HttpOptions("GET")
 );
 
@@ -13,13 +13,15 @@ if (handle != null) {
   Console.log(`code:${handle!}`);
   Console.log(handle!.getHeader("Content-Type")!);
   let body = handle!.getAllBody()!;
+  
   let jsonObj = <json.JSON.Obj>json.JSON.parse(body);
-  let arr = jsonObj.getArr("tokens");
-  if (arr != null) {
-    let vals = arr.valueOf();
-    vals.forEach((v) => {
-      Console.log(v.toString());
-    });
+  let kvs = jsonObj.valueOf();
+  if (kvs != null) {
+    let keys = kvs.keys();
+    for (let i = 0;  i < keys.length; i++)  {
+      let key = keys[i];
+      Console.log(`${key}: ${kvs.get(key)}`);
+    }
   }
   handle!.close();
 }
